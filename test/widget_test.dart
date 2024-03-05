7c8cc123ea9a129280589e8483f5b766054407e6
saakhani <muhammad.saad.002002@gmail.com>
2024-03-06 00:02:59 +0500

    Add main.dart and update imports

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:midterm/main.dart';
+import 'package:midterm/widgets/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
